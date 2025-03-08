5f631fe229db0746fb254682c6dad36019974b86
2005LIKHITH <likhithcvsrl@gmail.com>
2025-03-08 21:49:28 +0530

    Commit

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,10 @@ if (cluster.isPrimary) {
     res.send("Hello World!");
   });
 
+  app.get("/pid",(req,res)=>{
+    res.send("process id is "+process.pid)
+  })
+
   app.get("/api/:n", function (req, res) {
     let n = parseInt(req.params.n);
     let count = 0;
